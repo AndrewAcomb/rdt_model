492225f4fd48551e6cc6fce1d9ab84dc4a1b333c
Andrew Acomb <andrewacomb@Andrews-MacBook-Pro.local>
2018-06-20 01:33:43 -0700

    Parsed date vectors for speeches, created 'tweets', the final dataset by adding `speech`, a binary numeric vector that is 1 if trump had a speech that day, 0 otherwise.
     I will continue to add predictors to 'tweets'.

--- RDT_data_tidying.R
+++ RDT_data_tidying.R
@@ -2,17 +2,19 @@
 
 ## DATA TIDYING
 
-# Loading Packages
+## Loading Packages
 library(tidyverse)
 library(lubridate)
 
-# Loading Data
+## Loading Data
 rdt <- read_csv("Data/raw_data/rdt.csv")
 speeches <- read_delim("Data/raw_data/trump_speeches.csv", delim = "~")
 reddit <- read_delim("Data/raw_data/reddit_trump.csv", delim = "|")
 sp500 <- read_csv("Data/raw_data/GSPC.csv")
 
-# Parsing Date Vectors
+## Parsing Date Vectors
+
+# Parsing rdt dates and times
 
 a <- strsplit(rdt$created_at, " ") %>% unlist() %>% na.omit()
 b <- matrix(a, nrow = 2)
@@ -30,7 +32,23 @@ rdt <- rdt %>% select(-c(id_str, created_at))
 rdt <- rdt[1:16838,]
 
 rdt <- rdt %>% mutate(date = rdt_dates, time = rdt_times)
+rdt <- rdt %>% arrange(date,time)
+# Parsing speeches dates
 
+speeches$upload_date <- speeches$upload_date %>% parse_date("%Y%m%d")
+speech_day <- speeches$upload_date %>% day() %>% as.character()
+speech_month <- speeches$upload_date %>% month() %>% as.character()
+speech_year <- speeches$upload_date %>% year() %>% as.character()
+c <- paste(speech_year,speech_month, speech_day, sep = "-")  %>% parse_date("%Y-%m-%d")
+
+speeches <- speeches %>% select(-c(id, upload_date))
+speeches <- speeches %>% mutate(date = c)
+speeches <- speeches %>% arrange(date)
+
+
+## Joining Datasets
+
+tweets <- rdt %>% mutate(speech = if_else(date %in% speeches$date, 1, 0))
 
 
 # Trump watches Fox and Friends from 6-9 AM ET, often tweeting during the show. 
