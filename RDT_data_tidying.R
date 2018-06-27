fb9e75c914167ddb92b32fab9a85bccffc11399a
Andrew Acomb <andrewacomb@Andrews-MacBook-Pro.local>
2018-06-26 22:05:25 -0700

    Created a bunch of binary variables for whether certain words appear in Trump's schedule for that day.

--- RDT_data_tidying.R
+++ RDT_data_tidying.R
@@ -100,6 +100,24 @@ by_date %>% mutate(index = 1:nrow(by_date)) %>%
   ggplot(aes(index, tweet_count)) + geom_bar(stat = "identity") +
   xlab("days since 2014/1/1") + ylab("daily tweets")
 
+## Making multiple variables from schedule strings
 
-## Dealing with missing values
+sched_strings <- c("arrives", "departs", "press conference",
+   "THE FIRST LADY", "has no public events scheduled", "Pool Call Time",
+   "meets", "hosts", "signs", "intelligence briefing", "participates",
+   "In-Town", "Out-of-Town", "Vice President",
+   "Secretary of State", "Sarah Sanders", "remarks")
+
+# Function for detecting a specific string
+in_string <- function(x) {
+  if_else(str_detect(by_date$sched_text,x) ==T, 1, 0)
+}
+
+# Creating a matrix
+phrase_matrix <- sapply(sched_strings, in_string)
+phrase_df <- phrase_matrix %>% as_data_frame()
+colnames(phrase_df) <- paste0("sched_contains_", sched_strings)
+phrase_df <- phrase_df %>% mutate(date = by_date$date)
+# Adding to time series df
+by_date <- left_join(by_date, phrase_df)
 
